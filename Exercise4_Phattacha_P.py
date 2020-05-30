3a5170b2d6f6e5e76b6a8f6048718f34625b71f5
PTCM <iydcmhman1150@gmail.com>
2020-05-30 17:31:15 +0700

    Exercise4
    +++

---
+++ Exercise4_Phattacha_P.py
@@ -0,0 +1,10 @@
+eng = 72
+bis = 91
+comsys = 89
+compro = 95
+p = "Point"
+print("--- Your Score ---")
+print("Foundation English               :",eng,p)
+print("General Business                 :",bis,p)
+print("Introduction To Computer Systems :",comsys,p)
+print("Computer Programming             :",compro,p)
